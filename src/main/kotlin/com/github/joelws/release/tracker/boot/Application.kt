549e055b43f364ccab29c2b6684c5e92914af871
JoelW-S <jdws.dev@gmail.com>
2016-07-01 21:22:40 +0100

    Change to type safe config

--- src/main/kotlin/com/github/joelws/release/tracker/boot/Application.kt
+++ src/main/kotlin/com/github/joelws/release/tracker/boot/Application.kt
@@ -1,17 +1,20 @@
 package com.github.joelws.release.tracker.boot
 
+import com.github.joelws.release.tracker.configuration.AppConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.orm.jpa.EntityScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.ImportResource
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
 @EntityScan(basePackages = arrayOf("com.github.joelws.release.tracker.entity"))
-@EnableAutoConfiguration
+@Import(AppConfig::class)
+@EnableAutoConfiguration(exclude = arrayOf(AppConfig::class))
 @EnableTransactionManagement
-@ImportResource("classpath:META-INF/cxf/cxf.xml", "classpath:META-INF/cxf/cxf-servlet.xml", "classpath:META-INF/beans.xml")
+@ImportResource("classpath:META-INF/cxf/cxf.xml", "classpath:META-INF/cxf/cxf-servlet.xml")
 open class Application {
 
     companion object {
@@ -21,6 +24,7 @@ open class Application {
         }
 
     }
+
 
 }
 
